ccd2715f291dd905b4f7d8b06625e328ec697803
Ardonis Shalaj <ardonis.shalaj@outlook.com>
2023-12-31 11:44:41 +0100

    favicon done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,34 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#02041b"
+        />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`${open_sans.className} text-text bg-background antialiased`}>
         <Providers>
           <NavBar />
