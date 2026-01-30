c0b5d0655d3cfca76a793fdd51c4d01bdf9aeb6d
Ardonis Shalaj <ardonis.shalaj@outlook.com>
2026-01-30 21:19:10 +0100

    Refactor NavBar component for improved layout and styling, including background opacity adjustment and menu item rendering

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -33,23 +33,18 @@ export const NavBar = () => {
             height="6rem"
             classNames={{
                 wrapper: "max-w-[1600px] px-4 md:px-16 2xl:px-4 !h-16 min-h-16",
-                base: "z-50 !min-h-0 !bg-background/50",
+                base: "z-50 !min-h-0 !bg-background/70",
             }}
             onMenuOpenChange={setIsMenuOpen}
             isMenuOpen={isMenuOpen}
         >
-            <NavbarContent>
+            <NavbarContent justify="start" className="items-center gap-6">
                 <NavbarMenuToggle
                     aria-label={isMenuOpen ? "Close menu" : "Open menu"}
                     className="md:hidden"
                 />
                 <NavbarBrand className="">
                     <Link href="/">
-                        {/*<Logo_specific logoType="Logo_border_blue"
-                            complexity="navbar_footer"
-                            width='auto'
-                            customClasses="hidden lg:inline">
-        </Logo_specific>*/}
                         <Logo iconOnly={false} customClasses="hidden lg:inline" />
                         <Logo
                             iconOnly={false}
@@ -58,17 +53,13 @@ export const NavBar = () => {
                         />
                     </Link>
                 </NavbarBrand>
-                {menuItems.map((item, index) => {
-                    return (
-                        <NavbarItem
-                            className="mr-4 hidden md:flex h-full items-center justify-center"
-                            key={index}
-                        >
-                            <NavLink href={`${item.href}`} content={item.name} />
-                        </NavbarItem>)
-                })}
+                {menuItems.map((item, index) => (
+                    <NavbarItem key={index} className="hidden md:flex items-center">
+                        <NavLink href={`${item.href}`} content={item.name} />
+                    </NavbarItem>
+                ))}
             </NavbarContent>
-            <NavbarContent justify="end">
+            <NavbarContent justify="end" className="items-center">
                 <NavbarItem>
                     <PrimaryButton content="Contactez-nous !" href="/#contact" />
                 </NavbarItem>
